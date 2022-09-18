95e4f3ab2747f92e11736d14e1dfe3eb73828306
Esther Manrique <esther_manrique@epam.com>
2022-09-18 14:50:48 -0500

    Added home componet, routes and routes_paths

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,9 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:ngcomponents/angular_components.dart';
-import 'package:ngdart_app_courses/src/components/courses/courses_componet.dart';
+// import 'package:ngdart_app_courses/src/components/courses/courses_componet.dart';
 import 'package:ngdart_app_courses/src/components/header/header_component.dart';
+import 'package:ngdart_app_courses/src/utils/route_paths.dart';
 
 // AngularDart info: https://angulardart.xyz
 // Components info: https://angulardart.xyz/components
@@ -10,11 +12,15 @@ import 'package:ngdart_app_courses/src/components/header/header_component.dart';
 // https://pub.dev/ngcomponents)
 
 @Component(
-  selector: 'my-app',
-  styleUrls: ['app_component.css'],
-  templateUrl: 'app_component.html',
-  directives: [MaterialIconComponent, AppHeader, CoursesComponent],
-)
+    selector: 'my-app',
+    styleUrls: ['app_component.css'],
+    templateUrl: 'app_component.html',
+    directives: [
+      routerDirectives,
+      MaterialIconComponent,
+      AppHeader,
+    ],
+    exports: [RoutePaths, RoutePath])
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
 }
